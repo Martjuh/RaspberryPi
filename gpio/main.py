d8b62c4169d4cdba4e3b92f07bd4087269210c99
Martijn Jansen <jansenm92@gmail.com>
2022-11-19 15:26:08 +0100

    added cleanup

--- gpio/main.py
+++ gpio/main.py
@@ -7,14 +7,21 @@ except (ImportError, RuntimeError):
 import time
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.OUT)
-# loop through 50 times, on/off for 1 second
-for i in range(50):
-    GPIO.output(7,True)
-    time.sleep(1)
-    GPIO.output(7,False)
-    time.sleep(1)
-GPIO.cleanup()
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(7, GPIO.OUT)
+    # loop through 50 times, on/off for 1 second
+    for i in range(50):
+        GPIO.output(7,True)
+        time.sleep(1)
+        GPIO.output(7,False)
+        time.sleep(1)
+    GPIO.cleanup()
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+   print("Keyboard interrupt")
 
-print(test_environment)
+except:
+   print("some error")
+finally:
+   print("clean up")
+   GPIO.cleanup()
